53294dac856048930cc692a20824034aaf419091
John <johnaloomis@ufl.edu>
2017-09-06 14:15:23 -0400

    Interface Complete
    issues left:
    No lat,long, add in added builds
    
    delete button tied to index not listing next to it

--- listingController.js
+++ listingController.js
@@ -7,8 +7,31 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      Listings.unshift({'code' : $scope.code,
+	  'name' : $scope.name,
+	  'coordinates.latitude' : $scope.lati,
+	  'coordinates.longitude' : $scope.longi,
+	  'address' : $scope.addr});
+      
+    };
+    $scope.deleteListing = function(index) {
+		$scope.listings.splice(index,1);
+	};
+	
+    $scope.showDetails = function(index) {
+		
+		$scope.code = '';
+		$scope.name = '';
+		$scope.lati = '';
+		$scope.longi = '';
+		$scope.addr = '';
+		$scope.code = index.code;
+		$scope.name = index.name;
+		$scope.lati = index.coordinates.latitude;
+		$scope.longi = index.coordinates.longitude;
+		$scope.addr = index.address;
+	};
+	
   }
 ]);
